84298143c8fbc3b774e8e684a2337c267934381e
skywalker95 <lmdani95@yahoo.com>
2015-12-20 18:03:55 -0600

    IClass
    Finished the IClass interface -Luke

--- BlueShells374/src/problem/asm/IClass.java
+++ BlueShells374/src/problem/asm/IClass.java
@@ -1,5 +1,23 @@
 package problem.asm;
 
-public interface IClass {
+import java.util.Collection;
 
+public interface IClass {
+	public Collection<IMethod> getIMethods();
+	public Collection<IField> getIField();
+	public EClassAccess getAcessLevel();
+	public String getSignature();
+	public String getClassName();
+	public int getClassVersion();
+	public IClass getSuperName();
+	public Collection<Interface> getInterface();
+	public void addIMethods(IMethod method);
+	public void addIField(IField field);
+	public void setAccessLevel(EClassAccess access);
+	public void setSignature(String signature);
+	public void setClassName(String name);
+	public void setClassVersion(int version);
+	public void setSuperName(IClass name);
+	public void addInterface(Interface inter);
+	public String toString();
 }
