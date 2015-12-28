ef367cb401b55b85ac7f8ee0392da4d96fe40fea
Larry Gates <gateslm@rose-hulman.edu>
2015-12-28 12:49:30 -0500

    Started writing tests
    Began writing tests for field, just getting an idea of what I am going
    to write.

--- BlueShells374/test/problem/blueshells/testing/FieldTest.java
+++ BlueShells374/test/problem/blueshells/testing/FieldTest.java
@@ -6,20 +6,36 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import problem.asm.Field;
+import problem.asm.IField;
+
 public class FieldTest {
+	
+	private IField testField;
 
 	@Before
 	public void setUp() throws Exception {
-		// TODO
+		this.testField = new Field();
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		// TODO
+		this.testField = null;
+	}
+	
+	@Test
+	public void emptyTesting(){
+		assertEquals(this.testField.getName(), null);
+		assertEquals(this.testField.getDesc(), null);
+		assertEquals(this.testField.getSignature(), null);
+		assertEquals(this.testField.getValue() ,null);
+		assertEquals(this.testField.getAccessLevel(), null);
 	}
 
 	@Test
 	public void genericTesting() {
+		this.testField.setName("MULTIPLE_CONSTANT");
+		assertEquals(this.testField.getName(), "MULTIPLE_CONSTANT");
 		// FIXME
 		fail("Not yet implemented");
 	}
