e6ba7ee509eda013535511caf718cf22c600e01d
skywalker95 <lmdani95@yahoo.com>
2016-02-03 23:03:28 -0500

    Finished Composite Pattern Detection
    As above -Luke

--- BlueShells374/src/problem/visitor/CompositeVisitor.java
+++ BlueShells374/src/problem/visitor/CompositeVisitor.java
@@ -9,6 +9,7 @@ import problem.interfaces.IClass;
 import problem.interfaces.IField;
 import problem.interfaces.IMethod;
 import problem.interfaces.IModel;
+import problem.patterns.CompositePattern;
 
 /**
  * Refer to:<br/>
@@ -31,82 +32,175 @@ import problem.interfaces.IModel;
 public class CompositeVisitor implements IInvoker {
 
 	private IVisitor visitor;
-	private List<IClass> interfaces;
-	private Map<IClass, List<String>> abstractToInterface;
+	private Map<String, String> abstractToInterface; //extension, interface
 	private List<IClass> possibleComposites;
+	private List<String> possibleComponents;
 	private boolean notPossibleComposite;
+	private IClass currentClass;
 
 	public CompositeVisitor() {
 		this.visitor = new Visitor();
-		this.interfaces = new ArrayList<>();
 		this.abstractToInterface = new HashMap<>();
 		this.possibleComposites = new ArrayList<>();
+		this.possibleComponents = new ArrayList<>();
 		this.preVisitModel();
 		this.preVisitClass();
 		this.visitField();
 		this.visitMethods();
+		this.postVisitModel();
 	}
 
 	private void preVisitModel() {
 		this.visitor.addVisit(VisitType.PreVisit, IModel.class, (ITraverser t) -> {
 			IModel m = (IModel) t;
-			for (IClass c : m.getClasses()){
-				if (c.getAcessLevel() == 1537){
-					this.interfaces.add(c);
-				}
-				else if (c.getAcessLevel() == 1057) {
-					if (c.getInterface().size() > 0){
-						List<String> interfaces = new ArrayList<>();
-						for (String interfaze : c.getInterface()){
-							interfaces.add(interfaze);
+			for (IClass c : m.getClasses()) {
+				if (c.getAcessLevel() == 1057) {
+					if (c.getInterface().size() == 1) {
+						String inter = "";
+						for (String interfaze : c.getInterface()) {
+							inter = interfaze;
 						}
-						this.abstractToInterface.put(c, interfaces);
-					}
-					else {
-						List<String> noInterface = new ArrayList<>();
-						this.abstractToInterface.put(c, noInterface);
+						this.abstractToInterface.put(c.getClassName(), inter);
+					} else {
+						this.abstractToInterface.put(c.getClassName(), "");
 					}
 				}
 			}
-		}); 
+		});
 	}
-	
+
 	private void preVisitClass() {
-		this.visitor.addVisit(VisitType.PreVisit, IClass.class,
-				(ITraverser t) -> {
-					IClass c = (IClass) t;
-					this.notPossibleComposite = false;
-					if (c.getInterface().size() == 0 && c.getExtension().equals(null)){
-						this.notPossibleComposite = true;
-					}
-				});
+		this.visitor.addVisit(VisitType.PreVisit, IClass.class, (ITraverser t) -> {
+			IClass c = (IClass) t;
+			this.notPossibleComposite = false;
+			if (c.getInterface().size() == 0 && c.getExtension().equals(null)) {
+				this.notPossibleComposite = true;
+			}
+			this.currentClass = c;
+		});
 	}
-	
 
 	private void visitField() {
 		this.visitor.addVisit(VisitType.Visit, IField.class, (ITraverser t) -> {
 			IField f = (IField) t;
-			if (this.notPossibleComposite){
+			if (this.notPossibleComposite) {
 				return;
 			}
-			
-			if (f.getSignature().equals("")){
+			if (f.getSignature().equals("")) {
 				return;
 			}
-			System.out.println("sig " + f.getSignature());
-			System.out.println("class name" + this.interfaces.get(0).getClassName().toString());
+
+			String[] sig = f.getSignature().split("<");
+			String sigCheck = sig[sig.length - 1];
+			sigCheck = sigCheck.replace(".", "/");
+			sigCheck = sigCheck.substring(1);
+
+			for (String s : this.currentClass.getInterface()) {
+				if (s.equals(sigCheck)) {
+					this.possibleComposites.add(this.currentClass);
+					this.possibleComponents.add(sigCheck);
+				}
+			}
+
+			if (this.currentClass.getExtension().equals(sigCheck)) {
+				this.possibleComposites.add(this.currentClass);
+				this.possibleComponents.add(sigCheck);
+			}
+
+			if (this.abstractToInterface.containsKey(this.currentClass.getExtension())
+					&& this.abstractToInterface.containsValue(sigCheck)) {
+				this.possibleComposites.add(this.currentClass);
+				this.possibleComponents.add(sigCheck);
+			}
 		});
 
 	}
-	
-	private void visitMethods() {
-		this.visitor.addVisit(VisitType.Visit, IMethod.class,
-				(ITraverser t) -> {
-					if (this.notPossibleComposite){
-						return;
-					}
-				});
 
+	private void visitMethods() {
+		this.visitor.addVisit(VisitType.Visit, IMethod.class, (ITraverser t) -> {
+			if (this.notPossibleComposite) {
+				return;
+			}
+		});
+	}
+
+	private void postVisitModel() {
+		this.visitor.addVisit(VisitType.PostVisit, IModel.class, (ITraverser t) -> {
+			IModel m = (IModel) t;
+			List<String> comfComp = new ArrayList<>();
+			List<String> comfComposite = new ArrayList<>();
+			for (IClass c : m.getClasses()) {
+				for (String s : this.possibleComponents) {
+					if (c.getInterface().size() > 1 || c.getAcessLevel() == 1057) {
+						break;
+					}
+					List<String> classInterface = (List<String>) c.getInterface();
+					int iSize = classInterface.size();
+					String cExt = c.getExtension();
+					if ((iSize != 0 && classInterface.get(0).equals(s)) || cExt.equals(s)
+							|| (this.abstractToInterface.get(cExt) != null
+									&& this.abstractToInterface.get(cExt).equals(s))) {
+						// detect maybe leaf check fields
+						System.out.println("hit+++===+++=+++" + c.getClassName());
+						boolean hasComField = false;
+						for (IField f : c.getIField()){
+							if (hasComField){
+								break;
+							}
+							String[] sig = f.getSignature().split("<");
+							String sigCheck = sig[sig.length - 1];
+							sigCheck = sigCheck.replace(".", "/");
+							sigCheck = sigCheck.substring(1);
+							
+							if (sigCheck.equals(s)){
+								hasComField = true;
+							}
+						}
+						if (!hasComField){
+							System.out.println("hitLEAF");
+							if (!comfComp.contains(s)){
+								comfComp.add(s);
+							}
+							c.addPattern(new CompositePattern(c.getClassName(), "\\<\\<Leaf\\>\\>"));
+						}
+					}
+				}
+			}
+			
+			for (String s : comfComp){
+				for (IClass c : m.getClasses()){
+					if (c.getClassName().equals(s)){
+						c.addPattern(new CompositePattern(c.getClassName(), "\\<\\<Component\\>\\>"));
+					}
+					if (this.abstractToInterface.get(c.getClassName()) != null && this.abstractToInterface.get(c.getClassName()).equals(s)){
+						c.addPattern(new CompositePattern(c.getClassName(), "\\<\\<Component\\>\\>"));
+					}
+				}
+			}
+			
+			for (IClass s2 : this.possibleComposites){
+				List<IField> classFields = (List<IField>) s2.getIField();
+				for (IField f : classFields){
+					String[] sig = f.getSignature().split("<");
+					String sigCheck = sig[sig.length - 1];
+					sigCheck = sigCheck.replace(".", "/");
+					sigCheck = sigCheck.substring(1);
+					if (comfComp.contains(sigCheck)){
+						System.out.println("HITTTTTT");
+						s2.addPattern(new CompositePattern(s2.getClassName(), "\\<\\<Composite\\>\\>"));
+						comfComposite.add(s2.getClassName());
+					}
+				}
+			}
+			
+			for (IClass c2 : m.getClasses()){
+				for (String s3 : comfComposite){
+					if (c2.getExtension().equals(s3)){
+						c2.addPattern(new CompositePattern(c2.getClassName(), "\\<\\<Composite\\>\\>"));
+					}
+				}
+			}
+		});
 	}
 
 	@Override
