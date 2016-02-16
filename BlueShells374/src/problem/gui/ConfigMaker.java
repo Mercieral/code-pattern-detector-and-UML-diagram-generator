65e518e2ba75f3fa0a8d3cc29c93169efc69b478
Larry Gates <gateslm@rose-hulman.edu>
2016-02-16 00:59:43 -0500

    Config has more functionality

--- BlueShells374/src/problem/gui/ConfigMaker.java
+++ BlueShells374/src/problem/gui/ConfigMaker.java
@@ -1,13 +1,18 @@
 package problem.gui;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class ConfigMaker extends JPanel {
 
@@ -15,7 +20,7 @@ public class ConfigMaker extends JPanel {
 	 * Added by default
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	private String dirLocation;
 
 	public ConfigMaker() {
@@ -23,26 +28,39 @@ public class ConfigMaker extends JPanel {
 		JLabel title = new JLabel("Configuration Panel");
 		dirLocation = "";
 		titlePanel.add(title);
-		this.setLayout(new GridLayout(7, 6));
+		this.setLayout(new GridLayout(7, 3));
 		this.add(titlePanel);
 		findInputWindow();
 		addClassOptions();
 		findOutputWindow();
+		saveOutputName();
+	}
+
+	private void saveOutputName() {
+		JPanel panelButton = new JPanel();
+		panelButton.setSize(new Dimension(this.getWidth(), this.getHeight()));
+		JLabel outputFileName = new JLabel("Output File Name");
+		JTextField fileName = new JTextField();
+		fileName.setPreferredSize(new Dimension(250, (int) fileName.getPreferredSize().getHeight()));
+		panelButton.add(outputFileName);
+		panelButton.add(fileName);
+		fileName.setText("defaultFileName");
+		this.add(panelButton);
 	}
 
 	private void addClassOptions() {
 		JPanel panel = new JPanel();
-		JButton button = new JButton("Choose location");
+		JButton button = new JButton("Specify Classes to Use");
 		button.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if(dirLocation.equals("")){
+				if (dirLocation.equals("")) {
 					System.out.println("Location not choosen");
 				} else {
 					System.out.println("Location choosen");
 				}
-				
+
 			}
 		});
 		panel.add(button);
@@ -72,6 +90,7 @@ public class ConfigMaker extends JPanel {
 		FileFinderActionListener ffal = new FileFinderActionListener(
 				locationLabel, this, true);
 		button.addActionListener(ffal);
+
 		panelButton.add(label);
 		panelButton.add(button);
 		panelButton.add(locationLabel);
@@ -86,7 +105,8 @@ public class ConfigMaker extends JPanel {
 	}
 
 	/**
-	 * @param dirLocation the dirLocation to set
+	 * @param dirLocation
+	 *            the dirLocation to set
 	 */
 	public void setDirLocation(String dirLocation) {
 		this.dirLocation = dirLocation;
