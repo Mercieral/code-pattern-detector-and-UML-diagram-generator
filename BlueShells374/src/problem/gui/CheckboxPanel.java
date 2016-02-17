359835ed6101b75d1e5be73a6d62fe9823f58da9
Aaron Mercier <mercieal@rose-hulman.edu>
2016-02-16 23:55:49 -0500

    Checkboxes looking pretty dank
    Checkboxes looking pretty dank. The indents look good and the "title"
    checkbox/label is bigger and more noticeable. When they are clicked the
    appropriate info is printed to the console. This info will be used to
    update the image panel (hopefully)

--- BlueShells374/src/problem/gui/CheckboxPanel.java
+++ BlueShells374/src/problem/gui/CheckboxPanel.java
@@ -2,6 +2,14 @@ package problem.gui;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -23,18 +31,29 @@ public class CheckboxPanel extends JPanel {
 	List<String> classes;
 	
 	public CheckboxPanel(IModel model){
-		this.setPreferredSize(new Dimension(150,this.getHeight()));
+//		this.setPreferredSize(new Dimension(200 ,this.getHeight()));
+		this.setLayout(new GridBagLayout());
 		this.setBorder(BorderFactory.createMatteBorder(0, 0, 0, 2, Color.BLACK));
-		JLabel testLabel = new JLabel();
 		classes = new ArrayList<String>();
 		checkboxGroups = new HashMap<String, List<JCheckBox>>();
-		testLabel.setText("test Checkbox");
-		this.add(testLabel);
 		
 		//fills in map of CheckBoxes
+		this.fillMapWithCheckBoxes(model);
+		this.drawCheckBoxes();
+	}
+	
+	private void fillMapWithCheckBoxes(IModel model){
 		for (IClass c : model.getClasses()){
+			classes.add(c.getClassName().replace("/", "."));
 			JCheckBox classBox = new JCheckBox(c.getClassName());
 			classBox.setSelected(true);
+			classBox.addItemListener(new ItemListener() {
+				@Override
+				public void itemStateChanged(ItemEvent e) {
+					System.out.println("class " + classBox.getText() + " is now set to " + classBox.isSelected());
+				}
+			});
+			
 			if (c.getPatterns().isEmpty()){
 				if (checkboxGroups.containsKey("No Pattern")){
 					checkboxGroups.get("No Pattern").add(classBox);
@@ -55,6 +74,42 @@ public class CheckboxPanel extends JPanel {
 					}
 				}
 			}
+		}
+	}
+	
+	private void drawCheckBoxes(){
+		int counter = 0;
+		GridBagConstraints c = new GridBagConstraints();
+		c.anchor = GridBagConstraints.WEST;
+		for(String s : this.checkboxGroups.keySet()){
+			c.gridy = counter;
+			Font patternFont = new Font("Serif", Font.BOLD, 20);
+			if (s.equals("No Pattern")){
+				JLabel noPatternLabel = new JLabel("No Pattern");
+				noPatternLabel.setFont(patternFont);
+				this.add(noPatternLabel, c);
+			}
+			else{
+				JCheckBox patternBox = new JCheckBox(s);
+				patternBox.setSelected(true);
+				patternBox.setFont(patternFont);
+				patternBox.addItemListener(new ItemListener() {
+					@Override
+					public void itemStateChanged(ItemEvent e) {
+						System.out.println("pattern " + patternBox.getText() + " is now set to " + patternBox.isSelected());
+						patternBox.setFont(patternFont);
+					}
+				});
+				this.add(patternBox, c);
+			}
+			c.insets = new Insets(0,30,0,0);
+			for (JCheckBox box : this.checkboxGroups.get(s)){
+				counter++;
+				c.gridy = counter;
+				this.add(box, c);
+			}
+			c.insets = new Insets(0,0,0,0);
+			counter++;
 		}
 	}
 
